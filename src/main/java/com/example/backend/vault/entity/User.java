011f639446d4594d1db1a4350362f9811b70159a
zakaria <djgang013@gmail.com>
2026-03-10 00:45:40 +0000

    database configuration

--- src/main/java/com/example/backend/vault/entity/User.java
+++ src/main/java/com/example/backend/vault/entity/User.java
@@ -1,13 +1,17 @@
 package com.example.backend.vault.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter @Setter
 public class User {
     @Id
@@ -17,9 +21,9 @@ public class User {
     @Column(unique = true,nullable = false)
     private String username;
     @Column(nullable = false)
-    private String masterhash;
+    private String masterHash;
 
-    @Column(,nullable = false)
+    @Column(nullable = false)
     private String salt;
 
     private LocalDateTime createdAt = LocalDateTime.now();
